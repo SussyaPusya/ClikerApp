373cbf02520a151d7002931077b4b7f91e890903
susya pusya <mranimatory@gmail.com>
2024-09-16 19:22:37 +0300

    added music fixed bugs realized the inclusion of random tracks and connected muta button

--- cmd/web/coproscript.js
+++ cmd/web/coproscript.js
@@ -1,3 +1,5 @@
+
+
 document.getElementById("ClickButton").addEventListener("click", function() {
     fetch("http://localhost:3000/endpoint", {
         method: "POST", 
@@ -13,7 +15,8 @@ document.getElementById("ClickButton").addEventListener("click", function() {
     .catch((error) => {
         console.error("Ошибка:", error);
     });
-    console.log("+1 govno")
+    
+    
 });
 
 const numberElement = document.getElementById('count');
@@ -45,7 +48,7 @@ window.addEventListener('click', function(event) {
     }
 });
 
-
+const music = document.getElementById('music');
 const cnopka = document.getElementById('sound-button');
 const icon = document.getElementById('icon');
 
@@ -53,11 +56,46 @@ let isPlaying = false;
 
 function SwitchIcon(){
     if (isPlaying) {
+        music.volume = 0.1
         icon.src = '../web/assets/sound-svgrepo-com.svg';
     } else {
+        music.volume = 0
         icon.src = '../web/assets/sound-off-svgrepo-com.svg';
     }
     isPlaying = !isPlaying; 
 }
 cnopka.addEventListener('click', SwitchIcon);
+const tracks = [
+    '../web/assets/Disfigure - Blank.mp3',
+    '../web/assets/DifferentHeaven - MyHeart.mp3',
+    '../web/assets/Elektronomia - SkyHigh.mp3',
+    ];
+let x = 0;
+
+
+const clickButn = document.getElementById('ClickButton');
+clickButn.addEventListener('click', () => {
+        x++;
+        TogglePlay();
+});
+function TogglePlay(){
+    if (x == 1) {
+        const randomIndex = Math.floor(Math.random() * tracks.length);
+        music.src = tracks[randomIndex];
+        
+        music.play()
+        music.volume = 0.1
+    } else{
+        
+    }
+}
+const audio = document.getElementById('music');
+
+function loadRandomTrack() {
+        const randomIndex = Math.floor(Math.random() * tracks.length);
+        audio.src = tracks[randomIndex];
+        audio.play();
+}
+
+audio.addEventListener('ended', loadRandomTrack);
 
